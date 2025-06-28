9e9eb3c6f2c6e6e41b4a1836bad81a7a4c15046a
Ana-Maria Stratulat <a.m.stratulat@gmail.com>
2025-06-28 19:04:01 +0300

    Remove labels

--- src/components/NavbarSelectors.tsx
+++ src/components/NavbarSelectors.tsx
@@ -7,22 +7,22 @@ interface NavbarSelectorsProps {
 
 const NavbarSelectors: React.FC<NavbarSelectorsProps> = ({ navbarPosition, setNavbarPosition }) => (
     <div style={getSelectorContainerStyle(navbarPosition)}>
-        <label htmlFor="theme-select" style={{ marginRight: '5px' }}>Theme:</label>
+        <label htmlFor="theme-select" style={{ marginRight: '5px' }} />
         <select id="theme-select" onChange={(e) => switchTheme(e.target.value)} style={{ marginRight: '15px' }}>
             <option value="classic">Classic</option>
             <option value="minimal">Minimal</option>
         </select>
 
-        <label htmlFor="position-select" style={{ marginRight: '5px' }}>Position:</label>
+        <label htmlFor="position-select" style={{ marginRight: '5px' }} />
         <select
             id="position-select"
             value={navbarPosition}
             onChange={(e) => setNavbarPosition(e.target.value)}
         >
-            <option value="navbar-top">Top</option>
-            <option value="navbar-bottom">Bottom</option>
-            <option value="navbar-left">Left</option>
-            <option value="navbar-right">Right</option>
+            <option value="navbar-top">Top Menu</option>
+            <option value="navbar-bottom">Bottom Menu</option>
+            <option value="navbar-left">Left Menu</option>
+            <option value="navbar-right">Right Menu</option>
         </select>
     </div>
 );
